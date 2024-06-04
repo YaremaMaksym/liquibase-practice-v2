45d41764a24e868f2b9b4e8e6ea5d51a3e9420ae
Yarema Maksym <macsym2@gmail.com>
2024-06-04 17:18:18 +0300

    init project

---
+++ src/main/java/yaremax/com/liquibasepracticev2/LiquibasePracticeV2Application.java
@@ -0,0 +1,13 @@
+package yaremax.com.liquibasepracticev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasePracticeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibasePracticeV2Application.class, args);
+    }
+
+}
