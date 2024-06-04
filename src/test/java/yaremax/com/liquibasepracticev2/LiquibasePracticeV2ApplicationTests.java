8f56d8bd11b4e2afb6a8ebb236b429b40e83e13e
Yarema Maksym <macsym2@gmail.com>
2024-06-04 19:35:03 +0300

    add recreating db for tests

--- src/test/java/yaremax/com/liquibasepracticev2/LiquibasePracticeV2ApplicationTests.java
+++ src/test/java/yaremax/com/liquibasepracticev2/LiquibasePracticeV2ApplicationTests.java
@@ -3,7 +3,7 @@ package yaremax.com.liquibasepracticev2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.liquibase.drop-first=true")
 class LiquibasePracticeV2ApplicationTests {
 
     @Test
